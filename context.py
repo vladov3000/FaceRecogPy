d0847e38406723025354e74080016c9e17e3869d
vlad <vladov3000@gmail.com>
2020-10-17 22:27:07 -0700

    removed encodings folder from repo, auto creates saved encoding folder now

--- context.py
+++ context.py
@@ -3,3 +3,6 @@ import os
 
 BASE_PATH = os.path.dirname(os.path.realpath(__file__))
 SAVED_ENCODINGS_DIR = os.path.join(BASE_PATH, "saved_encodings")
+
+if not os.path.isdir(SAVED_ENCODINGS_DIR):
+    os.mkdir(SAVED_ENCODINGS_DIR)
